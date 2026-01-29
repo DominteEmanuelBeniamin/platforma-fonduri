78a58146af5ff8970685ef5621579cbc038953b9
tecs17 <sandu20321@gmail.com>
2026-01-29 20:05:41 +0200

    minor bug fix

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -42,10 +42,10 @@ export async function GET(
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const projectId = params.id
+    const {id: projectId} = await params
     if (!projectId) {
       return NextResponse.json({ error: 'Project ID lipsește din URL' }, { status: 400 })
     }
